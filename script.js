2fc6851c5844ea3750d11a026a81cb2d86e599c9
luisagallardov <luis.a.gallardo.v@gmail.com>
2026-07-25 17:00:34 -0700

    Add Mobile Hamburger Menu

--- script.js
+++ script.js
@@ -23,3 +23,12 @@
             openQuote.blur();
         }
     });
+
+    const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if (menuToggle && mobileMenu) {
+    menuToggle.addEventListener("click", () => {
+        mobileMenu.classList.toggle("open");
+    });
+}
